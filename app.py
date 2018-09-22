215947ac1bd9947cfb617cb5c26d8d78b2616677
software groups <sfgroups@gmail.com>
2018-09-21 23:45:21 -0400

    added health check page

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return Response("Hi from your Flask app running in your Docker container!")
-    
+
+@app.route("/healthz")
+def healthz():
+     return  Response("OK")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
